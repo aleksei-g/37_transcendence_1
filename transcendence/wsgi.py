334e7a20a5134e86dd42dd39fe0cec7a1b305ec9
aleksei-g <aleksei_g@outlook.com>
2018-02-25 15:42:50 +0300

    edit django management files as recommended django-configurations

--- transcendence/wsgi.py
+++ transcendence/wsgi.py
@@ -1,9 +1,8 @@
 import os
 
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcendence.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
